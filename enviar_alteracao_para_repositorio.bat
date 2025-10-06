3ffde3b49c4349c06852b5946f5ddedb8e2313b5
Camilla Vitoria Silva <camillav09@gmail.com>
2025-10-06 12:45:52 -0300

    Comando BAT para enviar alterações.

---
+++ enviar_alteracao_para_repositorio.bat
@@ -0,0 +1,17 @@
+echo OFF
+
+echo --- Preparar aquivos para subir ---
+
+git add .
+
+echo --- Mensagem para Deus ---
+
+git commit -m "Arquivos utilizados ou alterados em aula."
+
+echo --- Enviar para nuvem ---
+
+git push
+
+echo --- Fim ---
+
+pause
